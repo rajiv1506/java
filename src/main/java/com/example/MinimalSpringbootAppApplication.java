931b7a18b989e99bf9c8f3cfb0064c13e105e7e5
rajiv <rajiv.pratap26@gmail.com>
2025-09-20 14:33:47 +0530

    testing

---
+++ src/main/java/com/example/MinimalSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimalSpringbootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MinimalSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalSpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
