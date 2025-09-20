b65fb3e613c375a39927bebd4e5d6a587569c960
rajiv <rajiv.pratap26@gmail.com>
2025-09-20 14:39:59 +0530

    testing

---
+++ src/test/java/com/example/HelloControllerTest.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void helloEndpointReturnsHelloSpringBoot() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, Spring Boot!"));
+    }
+}
